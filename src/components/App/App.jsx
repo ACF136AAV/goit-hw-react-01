e1656553bb6b9473ecde7890ce56ea71b34d2a91
ACF136 <andrusacf@ukr.net>
2024-06-28 16:58:58 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-// Імпорт стилів нормалізації 10-05-2024
-// import "modern-normalize";
 import './App.css'
 
 import Profile from '../Profile/Profile';
